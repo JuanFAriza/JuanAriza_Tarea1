dbbfb69bc191eccb9e6e353c307405f3b4cde892
JuanFAriza <jf.ariza10@uniandes.edu.co>
2017-03-06 09:19:16 -0500

    Falta imprimir los valores de la energía en los primeros modos normales

--- fermiPasta.c
+++ fermiPasta.c
@@ -9,12 +9,14 @@
 #define T = 5.0*pow(N,2.2)
 #define instantaneas 1000 // Numero de instantaneas que se tomaran
 
-void segunda_derivada(double *x_tt, double *x, int n, double beta); // Devuelve la segunda derivada
+void segunda_derivada(double *x_tt, double *x); // Devuelve la segunda derivada
+void imprimir_energia(double *x, double *v); // Imprime la energia
 
 int main(){
-  int i,iter, Nt;
+  int i,iter, Nt, im;
 
   Nt = (int) T/delta_t;
+  
   double *x, *v, *x0, *v0, *vmedio, *a; // Posiciones, velocidad, pos ini, vel ini, vel en el medio, aceleracion
   x0 = malloc(N*sizeof(double)); // Crea posicion inicial
   v0 = malloc(N*sizeof(double)); // Crea velocidad inicial
@@ -25,30 +27,40 @@ int main(){
     x[i] = x0[i] = sin(Pi*i/((double)(N - 1)));
     v[i] = v0[i] = 0;
   }
+  x[0] = x[N-1] = 0;
 
-  for(iter=0;iter<Nt;iter++){
-    segunda_derivada(a,x,N,beta);
-    for(i=0;i<N;i++){
-      vmedio[i] = v[i] +0.5*delta_t*a[i];
+  imprimir_energia(x,v);
+
+  for(im=0;im<999;im++){
+    for(iter=0;iter<(Nt/1000);iter++){
+      segunda_derivada(a,x,N,beta);
+      for(i=0;i<N;i++){
+	vmedio[i] = v[i] +0.5*delta_t*a[i];
+      }
+      vmedio[0] = vmedio[N-1] = 0;
+      for(i=0;i<N;i++){
+	x[i] = x[i] + delta_t*vmedio[i];
+      }
+      x[0] = x[N-1] = 0;
+      segunda_derivada(a,x,N,beta);
+      for(i=0;i<N;i++){
+	v[i] = vmedio[i] + 0.5*delta_t*a[i];
+      }
+      v[0] = v[N-1] = 0;
     }
-    vmedio[0] = vmedio[N-1] = 0;
-    for(i=0;i<N;i++){
-      x[i] = x[i] + delta_t*vmedio[i];
-    }
-    segunda_derivada(a,x,N,beta);
-    for(i=0;i<N;i++){
-      v[i] = vmedio[i] + 0.5*delta_t*a[i];
-    }
+    imprimir_energia(x,v);
   }
   return 0;
 }
 
-void segunda_derivada(double *x_tt, double *x, int n, double beta){
+void segunda_derivada(double *x_tt, double *x){
   int i;
-  x_tt[0] = 0;
-  x_tt[n-1] = 0;
-
-  for(i=0;i<n-1;i++){
+  x_tt[0] = x_tt[N-1] = 0;
+  for(i=1;i<N-1;i++){
     x_tt[i] = (x[i+1] - 2*x[i] + x[i-1]) + beta*(pow((x[i+1] - x[i]),3) - pow((x[i] - x[i-1]),3));
   }
+}
+
+void imprimir_energia(double *x, double *v){
+  
 }
