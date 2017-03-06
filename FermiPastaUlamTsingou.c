84dff48f2f949f778f2e0222049222a1a5326c8a
Juan Felipe Ariza Mejia <jf.ariza10@uniandes.edu.co>
2017-03-06 13:52:41 -0500

    Faltan más pruebas, pero parece funcionar

--- FermiPastaUlamTsingou.c
+++ FermiPastaUlamTsingou.c
@@ -23,7 +23,7 @@ int main(int argc, char **argv){
   
   double *x, *v, *x0, *v0, *vmedio, *a; // Posiciones, velocidades, pos ini, vel ini, vel en el medio, aceleracion
   x = malloc(N*sizeof(double)); // Crea posiciones
-  v = mallov(N*sizeof(double)); // Crea velocidades
+  v = malloc(N*sizeof(double)); // Crea velocidades
   x0 = malloc(N*sizeof(double)); // Crea posicion inicial
   v0 = malloc(N*sizeof(double)); // Crea velocidad inicial
   vmedio = malloc(N*sizeof(double)); // Crea velocidades tras medio paso
@@ -40,7 +40,7 @@ int main(int argc, char **argv){
 
   for(im=1;im<1000;im++){
     for(iter=0;iter<Nt;iter++){
-      segunda_derivada(a,x,N,beta);
+      segunda_derivada(a,x);
 
 #pragma omp parallel for shared(v,vmedio,a)
       for(i=0;i<N;i++){
@@ -55,7 +55,7 @@ int main(int argc, char **argv){
       }
 
       x[0] = x[N-1] = 0;
-      segunda_derivada(a,x,N,beta);
+      segunda_derivada(a,x);
 
 #pragma omp parallel for shared(v,vmedio,a)
       for(i=0;i<N;i++){
@@ -83,23 +83,22 @@ void segunda_derivada(double *x_tt, double *x){
 void imprimir_energia(double *x, double *v, double t){
   int i,j;
   double A[3], Apunto[3], k[3], w_2[3], E[3];
-  k[0] = 1;
-  k[1] = 2;
-  k[2] = 3;
+
   for(j=0;j<3;j++){
     A[j] = 0;
     Apunto[j] = 0;
+    k[j] = j + 1;
     w_2[j] = 4*pow(sin(k[j]*Pi/(2*N + 2)),2);
 
 #pragma omp parallel for shared(A,Apunto,x,v,k)
     for(i=0;i<N;i++){
-      A[j] = A[j] + x[i]*sin(i*k[j]*Pi/(N+1));
-      Apunto[j] = Apunto[j] + v[i]*sin(i*k[j]*Pi/(N+1));
+      A[j] += x[i]*sin(i*k[j]*Pi/(N+1));
+      Apunto[j] += v[i]*sin(i*k[j]*Pi/(N+1));
     }
 
-    A[j] = A[j]*pow((2/(N+1)),0.5);
-    Apunto[j] = Apunto[j]*pow((2/(N+1)),0.5);
-    E[j] = (pow(Apunto[j],2) + w_2[j]*pow(A[j],2))/2
+    A[j] = A[j]*pow((2.0/(N+1)),0.5);
+    Apunto[j] = Apunto[j]*pow((2.0/(N+1)),0.5);
+    E[j] = (pow(Apunto[j],2) + w_2[j]*pow(A[j],2))/2;
   }
-  printf("%f %f %f %f",t,E[0],E[1],E[2]);
+  printf("%f %f %f %f\n",t,E[0],E[1],E[2]);
 }
